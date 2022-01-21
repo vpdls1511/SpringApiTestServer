2350c6ed9a227e5a462e91a26a04ae2e73b761e5
vpdls1511 <vpdls1511@gmail.com>
2022-01-21 18:57:39 +0900

    프로젝트 생성

---
+++ src/main/java/com/ngyu/springapitestserver/SpringApiTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.ngyu.springapitestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiTestServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ngyu/springapitestserver/SpringApiTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngyu.springapitestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
