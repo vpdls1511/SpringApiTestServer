9007d8ed9282464db2cc18f12909d8461ab6dcbb
nGyu <vpdls1511@gmail.com>
2022-02-04 16:01:37 +0900

    Jackson을 이용한 JSON 응답

--- src/main/java/com/ngyu/springapitestserver/module/user/domain/RegisterRequest.java
+++ src/main/java/com/ngyu/springapitestserver/module/user/domain/RegisterRequest.java
@@ -10,14 +10,7 @@ import javax.validation.constraints.*;
 public class RegisterRequest {
     //TODO : 회원가입 시 받을 값 정하고, DTO 개념 공부 후 적용하기
 
-    @NotEmpty(message = "이름 입력은 필수 입니다.")
     private String userName;
-
-    @NotEmpty(message = "이메일 입력은 필수 입니다.")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String userEmail;
-
-    @NotEmpty(message = "비밀번호 입력은 필수 입니다.")
-    @Size(min = 5, message = "비밀번호는 최소 5자 이상이어야 합니다.")
     private String userPassword;
 }
--- src/main/java/com/ngyu/springapitestserver/module/user/rest/UserController.java
+++ src/main/java/com/ngyu/springapitestserver/module/user/rest/UserController.java
@@ -1,18 +1,31 @@
 package com.ngyu.springapitestserver.module.user.rest;
 
-import com.ngyu.springapitestserver.module.user.domain.RegisterRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.ngyu.springapitestserver.module.user.dto.RegisterDTO;
 import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+    @GetMapping("/api/user")
+    public RegisterDTO getUser(){
+        RegisterDTO reg = RegisterDTO.builder()
+                .userName("name")
+                .userPassword("password")
+                .userEmail("email")
+                .userAge(23)
+                .userGender("male")
+                .build();
+        return reg;
+    }
+
     @PostMapping("/api/user")
-    public ResponseEntity<?> getUser(@Validated RegisterRequest userReq, Errors errors){
-        if(errors.hasErrors()) return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity(HttpStatus.OK);
+    public String setUser(
+            @RequestBody RegisterDTO regDto,
+            Errors errors
+    ){
+        if(errors.hasErrors()) return "ERR";
+        return "TEST";
     }
 }
