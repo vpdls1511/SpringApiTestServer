4146afa627a73812f73f561df16fba45e2328a72
nGyu <vpdls1511@gmail.com>
2022-02-06 14:51:40 +0900

    테스트케이스 업로드

--- src/main/java/com/ngyu/springapitestserver/module/user/rest/UserController.java
+++ src/main/java/com/ngyu/springapitestserver/module/user/rest/UserController.java
@@ -1,13 +1,18 @@
 package com.ngyu.springapitestserver.module.user.rest;
 
 import com.ngyu.springapitestserver.module.user.dto.RegisterDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class UserController {
+    //TODO - 로그인 테스트케이스 작성하기
     @GetMapping("/api/user")
     public RegisterDTO getUser(){
         RegisterDTO reg = RegisterDTO.builder()
@@ -20,12 +25,13 @@ public class UserController {
         return reg;
     }
 
+    //TODO - 회원가입 테스트케이스 작성하기
     @PostMapping("/api/user")
-    public String setUser(
-            @RequestBody RegisterDTO regDto,
+    public ResponseEntity setUser(
+            @RequestBody @Valid RegisterDTO regDto,
             Errors errors
     ){
-        if(errors.hasErrors()) return "ERR";
-        return "TEST";
+        if(errors.hasErrors()) return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(regDto);
     }
 }
---
+++ src/test/java/com/ngyu/springapitestserver/module/user/rest/UserControllerTest.java
@@ -0,0 +1,46 @@
+package com.ngyu.springapitestserver.module.user.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 1")
+    void signupTest() throws Exception {
+        Map<String,Object> input = new HashMap<>();
+
+        input.put("userName","김남규");
+        input.put("userEmail","vpdls1511@gmail.com");
+        input.put("userPassword","Test1234!!");
+        input.put("userAge",23);
+        input.put("userGender","Male");
+
+        mockMvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
